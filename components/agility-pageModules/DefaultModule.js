448b2920f82348e941b637f4ce4f8289fb0f5f89
Arjuna Shankar <ashankar@dotfusion.com>
2021-08-17 11:30:59 -0400

    Update DefaultModule.js

--- components/agility-pageModules/DefaultModule.js
+++ components/agility-pageModules/DefaultModule.js
@@ -1,7 +1,7 @@
 const DefaultModule = (props) => {
     const {module} = props;
     const {fields, properties} = module;
-    return <div>HELLO DEVELOPER! Component - {properties.definitionName} not defined</div>
+    return <div>HELLO DEVELOPER - this is only on a feature branch! Component - {properties.definitionName} not defined</div>
 }
 
 export default DefaultModule;
